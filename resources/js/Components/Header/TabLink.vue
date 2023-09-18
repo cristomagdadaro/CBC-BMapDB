551b34e6be2bedb4b6719da626a6e2e177bba312
LordyCristo <cristoreymagdadaro20@gmail.com>
2023-09-18 08:49:39 +0800

    Navbar

---
+++ resources/js/Components/Header/TabLink.vue
@@ -0,0 +1,20 @@
+<script>
+import { Link } from '@inertiajs/vue3';
+export default {
+    components: {
+        Link,
+    },
+    props: {
+        link: {
+            type: String,
+            required: true,
+            default: '#',
+        },
+    },
+};
+</script>
+<template>
+    <li class="flex items-center hover:bg-cbc-yellow-green duration-400">
+        <Link :href="link" class="px-3 py-1 text-gray-700 whitespace-nowrap sm:text-base text-xs"><slot /></Link>
+    </li>
+</template>
