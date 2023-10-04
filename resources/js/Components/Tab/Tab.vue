957e6a983162e50de51be140b9e4b0b24c7f0a38
lord <66482180+cristomagdadaro@users.noreply.github.com>
2023-10-04 17:10:48 +0800

    Tab component

---
+++ resources/js/Components/Tab/Tab.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {onMounted, ref} from "vue";
+
+defineProps({
+    tabs: Array|Object,
+})
+const activeTab = ref(null);
+
+const setActiveTab = function (tab) {
+    activeTab.value = tab;
+};
+
+//function that check if the active tab is the current tab
+const isActiveTab = function (tab) {
+    return activeTab.value === tab;
+};
+</script>
+<template>
+    <div class="flex flex-col">
+        <div class="flex gap-0.5">
+            <button class="py-1 px-2 bg-gray-300 text-gray-700 rounded-t-sm text-sm font-medium hover:bg-gray-400 active:bg-gray-500 active:text-gray-200 duration-300" v-for="tab in tabs" type="button" @click="setActiveTab(tab)">{{  tab.label }}</button>
+        </div>
+        <div v-if="activeTab" class="sm:px-4 py-2 p-1 bg-white rounded-b-md">
+            <slot :name="activeTab.name"></slot>
+        </div>
+    </div>
+</template>
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -9,6 +9,7 @@ import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import PageLayout from "@/Layouts/PageLayout.vue";
 import Logo from "@/Components/Icons/Logo.vue";
+import FullscreenToggle from "@/Components/FullscreenToggle.vue";
 
 defineProps({
     title: String,
@@ -51,6 +52,7 @@ const logout = () => {
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
+                            <FullscreenToggle />
                             <!-- Settings Dropdown -->
                             <div class="ml-3 relative">
                                 <Dropdown align="right" width="48">
@@ -58,7 +60,6 @@ const logout = () => {
                                         <span class="inline-flex rounded-md">
                                             <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                                 Settings
-
                                                 <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                                 </svg>
