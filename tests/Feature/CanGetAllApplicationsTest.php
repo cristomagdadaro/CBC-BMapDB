4ad68f3c9203b0cc779c06d9423a66d1ee00f7fc
lord <66482180+cristomagdadaro@users.noreply.github.com>
2023-10-17 09:54:54 +0800

    Added BaseController and GetApplicationRequest

--- tests/Feature/CanGetAllApplicationsTest.php
+++ tests/Feature/CanGetAllApplicationsTest.php
@@ -8,13 +8,21 @@ use Tests\TestCase;
 
 class CanGetAllApplicationsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    /** @test **/
+    public function it_can_get_all_applications(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/applications');
 
         $response->assertStatus(200);
+        $this->assertCount(10, $response['data']);
+    }
+
+    /** @test **/
+    public function it_can_search_on_all_searchable_fields(): void
+    {
+        $response = $this->get('/api/applications?search=1');
+
+        $response->assertStatus(200);
+        $this->assertCount(10, $response['data']);
     }
 }
