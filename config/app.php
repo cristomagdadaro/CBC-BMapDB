5ea7869747fbdf658a8641400bb3294b352eed95
lord <cristoreymagdadaro20@gmail.com>
2025-02-20 20:25:26 +0800

    Removed env in the main branch

--- config/app.php
+++ config/app.php
@@ -28,7 +28,7 @@ return [
     |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env.encrypted.production" file.
+    | services the application utilizes. Set this in your ".env.encrypted.local.production" file.
     |
     */
 
